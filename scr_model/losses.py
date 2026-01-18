31e68685f525082f304dd398b350da0a31c1f65f
jahneschuetz <zckm8t82v7@privaterelay.appleid.com>
2026-01-18 18:39:31 +0100

    added masking for loss function

--- scr_model/losses.py
+++ scr_model/losses.py
@@ -8,3 +8,12 @@ def cubic_mse(y_true, y_pred):
     """
     error = tf.abs(y_true - y_pred)
     return tf.reduce_mean(tf.pow(error, 3))
+
+@tf.function
+def cubic_mse_masked(y_true, y_pred, mask_value=-999.0):
+    mask = tf.not_equal(y_true, mask_value)           # True, wo y_true != -999
+    mask = tf.cast(mask, tf.float32)
+    
+    error = tf.abs(y_true - y_pred) ** 3
+    error = error * mask                              # maskierte Werte = 0
+    return tf.reduce_sum(error) / tf.reduce_sum(mask) # Mittelwert nur über echte Werte
