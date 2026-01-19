35d1da8d6ef4e01e8bd057ae1e7074911890a84a
jahneschuetz <zckm8t82v7@privaterelay.appleid.com>
2026-01-19 09:16:42 +0100

    fix cubic loss function

--- scr_model/losses.py
+++ scr_model/losses.py
@@ -16,4 +16,7 @@ def cubic_mse_masked(y_true, y_pred, mask_value=-999.0):
     
     error = tf.abs(y_true - y_pred) ** 3
     error = error * mask                              # maskierte Werte = 0
+
+    denom = tf.reduce_sum(mask)
+    denom = tf.maximum(denom, 1.0)   # verhindert Division durch 0
     return tf.reduce_sum(error) / tf.reduce_sum(mask) # Mittelwert nur über echte Werte
